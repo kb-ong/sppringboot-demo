9fca94db0a320f34d1d42dcb54b2833656a0aeec
kb-ong <93047957+kb-ong@users.noreply.github.com>
2022-02-13 01:04:47 +0800

    Add files via upload

---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+    @RequestMapping("/hello")
+    // Method
+    public String helloWorld()
+    {
+ 
+        // Print statement
+        return "Hello World!";
+    }
+
+}
